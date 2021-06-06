7590068daad14b9f8c43401a22884921c26e5d71
uliankaqa <uliankaqa@gmail.com>
2021-06-06 20:45:43 +0200

    update for HeaderTest class
    add method clickOnButton

--- src/test/java/com/ilCarro/qa14/HeaderTest.java
+++ src/test/java/com/ilCarro/qa14/HeaderTest.java
@@ -1,15 +1,33 @@
 package com.ilCarro.qa14;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class HeaderTest extends TestBase{
     @Test
     public void headerButtonCklick(){
-        driver.findElement(By.xpath("//ul[@class=\"header__nav desktop\"]//li//a[contains(.,'Search')]")).click();
-        driver.findElement(By.xpath("//ul[@class=\"header__nav desktop\"]//li//a[contains(.,'Let the cat work')]")).click();
-        //driver.findElement(By.xpath("//ul[@class=\"header__nav desktop\"]//li//a[contains(.,'Terms of use')]")).click();
-        driver.findElement(By.xpath("//ul[@class=\"header__nav desktop\"]//li//a[contains(.,'Sign up')]")).click();
-        driver.findElement(By.xpath("//ul[@class=\"header__nav desktop\"]//li//a[contains(.,'Log in')]")).click();
+
+        //Click on the [Search] button
+        clickOnButton(By.xpath("//ul[@class=\"header__nav desktop\"]//li//a[contains(.,'Search')]"));
+
+        //Click on the [let the car work] button
+        clickOnButton(By.xpath("//ul[@class=\"header__nav desktop\"]//li//a[contains(.,'Let the cat work')]"));
+
+       //Click on [Terms of use] button,
+        //Commented, failed the test
+        //clickOnButton(By.xpath("//ul[@class=\"header__nav desktop\"]//li//a[contains(.,'Terms of use')]"));
+
+
+        //Click on [Sign up] button
+        clickOnButton(By.xpath("//ul[@class=\"header__nav desktop\"]//li//a[contains(.,'Sign up')]"));
+
+        //Click on [Log in] button
+        clickOnButton(By.xpath("//ul[@class=\"header__nav desktop\"]//li//a[contains(.,'Log in')]"));
+    }
+    private void clickOnButton(By locator){
+
+        driver.findElement(locator).click();
+        Assert.assertTrue(isElementPresent(locator));
     }
 }
